58fcd71f8c18364289010dad2898dbba7e24afcc
HYEONSU.KWON <helloworld.hellohyeon@gmail.com>
2019-06-26 21:29:46 +0900

    db connect change

--- default.php
+++ default.php
@@ -4,6 +4,6 @@ header('Content-Type: text/html; charset=UTF-8');
 error_reporting(E_ERROR | E_PARSE);
 
 // mysql 로그인 명령문 있는 default.php 파일을 모든 파일에 include
-$connect = mysqli_connect('localhost', 'root', '000000', 'readingdiary', 3307);
+$connect = mysqli_connect('localhost', 'root', '1234', 'readingdiary', 3307);
 
 ?>
